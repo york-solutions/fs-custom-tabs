c4ebb14672b60f39755232d6ec38e0b0601fe4cb
Justin <justincyork@gmail.com>
2017-07-12 16:44:22 -0500

    detect when we're on a person page

--- src/tabs.js
+++ src/tabs.js
@@ -1,1 +1,46 @@
-console.log('FS Custom Tabs!');
+// Poll for URL changes.
+// The Family Tree uses he HTML5 History API but that API
+// doesn't fire events for pushState so we can't detect all
+// changes without polling.
+onURLChange(areWeReady);
+
+areWeReady();
+
+/**
+ * Decide what to do when the page loads and when the URL changes
+ */
+function areWeReady() {
+  if(isPersonPage()) {
+    console.log('on a person page');
+  } else {
+    console.log('not on a person page');
+  }
+}
+
+/**
+ * Check to see if we're on a person page
+ * 
+ * @returns {Boolean}
+ */
+function isPersonPage() {
+  return window.location.pathname.indexOf('/tree/person/') === 0;
+}
+
+/**
+ * Poll for URL changes.
+ * 
+ * @param {Function} callback Function called when a URL change is detected
+ */
+function onURLChange(callback) {
+  
+  // Cache the current value
+  var path = window.location.pathname;
+
+  // Setup the polling
+  window.setInterval(function() {
+    if (window.location.pathname !== path) {
+      path = window.location.pathname;
+      callback();
+    }
+  }, 500);
+}
