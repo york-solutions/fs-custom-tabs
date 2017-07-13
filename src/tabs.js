b1911a3eca9cff0d4e5038d63301b9939e8f1774
Justin <justincyork@gmail.com>
2017-07-13 11:10:05 -0500

    track when PID changes

--- src/tabs.js
+++ src/tabs.js
@@ -1,20 +1,44 @@
+// ID of the person page we're currently looking at.
+// This helps us detect when the tabs will be rendered.
+// Tabs are rendered when the ID changes.
+var personId = null;
+
 // Poll for URL changes.
 // The Family Tree uses he HTML5 History API but that API
 // doesn't fire events for pushState so we can't detect all
 // changes without polling.
-onURLChange(areWeReady);
+onURLChange(update);
 
-areWeReady();
+update();
 
 /**
  * Decide what to do when the page loads and when the URL changes
  */
-function areWeReady() {
-  if(isPersonPage()) {
-    console.log('on a person page');
-  } else {
-    console.log('not on a person page');
+function update() {
+  var newPersonId = getPersonPageId();
+
+  // No longer on a person page
+  if(newPersonId === null) {
+    console.log('not a person page');
   }
+
+  // Switched to a different person page
+  else if(newPersonId !== personId) {
+    console.log('new person page', newPersonId);
+    personId = newPersonId;
+    // TODO: detect when tabs are rendered
+  }
+}
+
+/**
+ * Get the ID of the person when on a person page.
+ * Otherwise return null.
+ */
+function getPersonPageId() {
+  if(isPersonPage()) {
+    return window.location.pathname.split('/')[3];
+  }
+  return null;
 }
 
 /**
