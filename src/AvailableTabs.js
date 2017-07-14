2e63fca5ec0475966a1fed9be96ad08b3026da56
Justin <justincyork@gmail.com>
2017-07-14 11:29:42 -0500

    forgot to add new files :)

---
+++ src/AvailableTabs.js
@@ -0,0 +1,11 @@
+var AvailableTabs = [
+
+  {
+    "id": "google",
+    "icon": "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_120x44dp.png",
+    "title": "Google",
+    "description": "Search Google for information about your ancestors.",
+    "url": "https://www.google.com/#q={pid}"
+  }
+
+];
---
+++ src/CustomTab.js
@@ -0,0 +1,27 @@
+class CustomTab extends Base {
+
+  constructor(data) {
+    super();
+    this.data = JSON.parse(JSON.stringify(data));
+  }
+
+  render() {
+    var tab = document.createElement('div');
+    tab.classList.add('available-tab');
+
+    var icon = document.createElement('img');
+    icon.src = this.data.icon;
+    tab.appendChild(icon);
+
+    var descr = document.createElement('p');
+    descr.textContent = this.data.description;
+    tab.appendChild(descr);
+
+    var button = document.createElement('button');
+    button.textContent = '+';
+    tab.appendChild(button);
+
+    return tab;
+  }
+
+}
